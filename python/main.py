010869dde4fbba985f3e57bc93a81bf13a8479d0
Jarrett Cheung <jarrettcheung27@gmail.com>
2026-03-06 12:20:41 +0800

    微调编码参数：迭代次数、epsilon。

--- python/main.py
+++ python/main.py
@@ -12,36 +12,53 @@ from Inner_Code_DNA_Channel_Simulation import DNAChannel
 import os
 import csv
 
-CODE_PARM = 8  # i for 2^i-ary LDPC, i=1, 2, 4 for 2-ary, 4-ary, 16-ary respectively
+CODE_PARM = 4  # i for 2^i-ary LDPC, i=1, 2, 4 for 2-ary, 4-ary, 16-ary respectively
 CODE_ARY = 2 ** CODE_PARM
-Parity_files_folder = "Parity_files_2048"
+CODE_LEN = 2048
+Parity_files_folder = f"Parity_files_{CODE_LEN}"
 def main():
+    if Parity_files_folder == "Parity_files_8320":
     # code parameter file name
-    if CODE_PARM == 1:
-        parity_filename = f"{Parity_files_folder}/8320_1280_2ary.dat"
-        mapping_filename = "Mapping_files/SignalSet_BPSK-1.txt"
-    elif CODE_PARM == 2:
-        parity_filename = f"{Parity_files_folder}/4160_640_4ary.dat"
-        mapping_filename = "Mapping_files/SignalSet_BPSK-2.txt"
-    elif CODE_PARM == 4:
-        parity_filename = f"{Parity_files_folder}/2080_320_16ary.dat"
-        mapping_filename = "Mapping_files/SignalSet_BPSK-4.txt"
-    elif CODE_PARM == 5:
-        parity_filename = f"{Parity_files_folder}/1664_256_32ary.dat"
-        mapping_filename = "Mapping_files/SignalSet_BPSK-5.txt"
-    elif CODE_PARM == 8:
-        parity_filename = f"{Parity_files_folder}/1040_160_256aryCode12.dat"
-        mapping_filename = "Mapping_files/SignalSet_BPSK-8.txt"
+        if CODE_PARM == 1:
+            parity_filename = f"{Parity_files_folder}/8320_1280_2ary.dat"
+            mapping_filename = "Mapping_files/SignalSet_BPSK-1.txt"
+        elif CODE_PARM == 2:
+            parity_filename = f"{Parity_files_folder}/4160_640_4ary.dat"
+            mapping_filename = "Mapping_files/SignalSet_BPSK-2.txt"
+        elif CODE_PARM == 4:
+            parity_filename = f"{Parity_files_folder}/2080_320_16ary.dat"
+            mapping_filename = "Mapping_files/SignalSet_BPSK-4.txt"
+        elif CODE_PARM == 5:
+            parity_filename = f"{Parity_files_folder}/1664_256_32ary.dat"
+            mapping_filename = "Mapping_files/SignalSet_BPSK-5.txt"
+        elif CODE_PARM == 8:
+            parity_filename = f"{Parity_files_folder}/1040_160_256aryCode12.dat"
+            mapping_filename = "Mapping_files/SignalSet_BPSK-8.txt"
+        else:
+            raise ValueError(f"Unsupported CODE_ARY: {CODE_ARY}")
     else:
-        raise ValueError(f"Unsupported CODE_ARY: {CODE_ARY}")
-    max_iteration = 10
+        if CODE_PARM == 1:
+            parity_filename = f"{Parity_files_folder}/2048_1024_2ary.dat"
+            mapping_filename = "Mapping_files/SignalSet_BPSK-1.txt"
+        elif CODE_PARM == 2:
+            parity_filename = f"{Parity_files_folder}/1024_512_4aryCode15.dat"
+            mapping_filename = "Mapping_files/SignalSet_BPSK-2.txt"
+        elif CODE_PARM == 4:
+            parity_filename = f"{Parity_files_folder}/512_256_16aryCode17.dat"
+            mapping_filename = "Mapping_files/SignalSet_BPSK-4.txt"
+        elif CODE_PARM == 8:
+            parity_filename = f"{Parity_files_folder}/256_128_256aryCode13.dat"
+            mapping_filename = "Mapping_files/SignalSet_BPSK-8.txt"
+        else:
+            raise ValueError(f"Unsupported CODE_ARY: {CODE_ARY}")
+    max_iteration = 50
     k_2 = 320  # inner code length of data bits
     # DNA channel parameters
     # PEs = np.linspace(0.05, 0.12, 8)  # different base error rates
-    PEs = [0.12]
-    sequencingDepths = [15]  # sequencing depth list
+    PEs = [0.08]
+    sequencingDepths = [20]  # sequencing depth list
     innerRedundancy = 114  # total redundancy for inner code
-    repeat_times = 20  # repeat simulation times for each sequencing depth
+    repeat_times = 50  # repeat simulation times for each sequencing depth
 
     codec = fftqspa.BCJRQSPA(parity_filename, max_iteration, mapping_filename)
 
@@ -52,8 +69,8 @@ def main():
         os.makedirs('results')
 
     for sequencingDepth in sequencingDepths:
-        runs_filename = f'results/FFTQSPA_DNA_Channel_{CODE_ARY}-ary_SequencingDepth{sequencingDepth}_InnerRedundancy{innerRedundancy}_runs.csv'
-        results_filename = f'results/FFTQSPA_DNA_Channel_{CODE_ARY}-ary_SequencingDepth{sequencingDepth}_InnerRedundancy{innerRedundancy}.csv'
+        runs_filename = f'results/codelen_{CODE_LEN}/FFTQSPA_DNA_Channel_{CODE_ARY}-ary_SequencingDepth{sequencingDepth}_InnerRedundancy{innerRedundancy}_runs_test3.csv'
+        results_filename = f'results/codelen_{CODE_LEN}/FFTQSPA_DNA_Channel_{CODE_ARY}-ary_SequencingDepth{sequencingDepth}_InnerRedundancy{innerRedundancy}_test.csv'
 
         with open(runs_filename, 'w', newline='') as f:
             writer = csv.writer(f)
@@ -68,7 +85,7 @@ def main():
                 info_bits = rng.integers(0, 2, size=(n_info, k_2), dtype=np.uint8)
 
                 # 分别对每一条信息比特进行编码
-                print("Q-ary LDPC encoding...")
+                print(f"{CODE_ARY}-ary LDPC encoding...")
                 code_bits = np.empty((n_code, k_2), dtype=np.uint8)
                 for i in range(k_2):
                     code_bits[:, i] = codec.encoder4bibo(info_bits[:, i])
@@ -77,15 +94,22 @@ def main():
                 # 输出voting scores 为多数投票得分, 也是就在一个簇中1的占比（约等于P(b=1)），loss sequence的得分为0.5。
                 voting_scores = DNAChannel(code_bits, Pe, sequencingDepth, innerRedundancy)  # shape: (n_code, k_2)
 
+                # 将 voting score 以 0.5 为阈值转换为2进制，并与内码输入的 比特流比较计算ber
+                hard_decision = (voting_scores >= 0.5).astype(np.uint8)
+                bit_errors_voting = np.sum(hard_decision != code_bits)
+                ber_voting = bit_errors_voting / code_bits.size
+                print(f"Voting score hard-decision BER: {ber_voting}")
+
+
                 # 计算P(b=0)
                 rr_bits_prob = 1 - voting_scores  # P(b=0)
 
                 # 为rr_bits_prob加一个微小值，防止出现0或1的概率，导致LLR无穷大
-                epsilon = 1e-5
+                epsilon = 0.05
                 rr_bits_prob = np.clip(rr_bits_prob, epsilon, 1 - epsilon)
 
                 # 分别对每一条码字进行译码
-                print("Q-ary LDPC decoding...")
+                print(f"{CODE_ARY}-ary LDPC decoding...")
                 decoded_bits = np.empty((n_code, k_2), dtype=np.uint8)
                 iters = np.empty(k_2, dtype=int)
                 for i in range(k_2):
@@ -106,6 +130,10 @@ def main():
                 BER = total_bit_errors / (n_info * k_2)
                 FER = total_frame_errors / n_info
                 print(f"Run {run_idx}, Pe={Pe}: BER={BER}, FER={FER}")
+
+                # 打印出出错的比特在 LDPC 译码时的帧的位置，以及迭代次数。
+                
+
 
                 with open(runs_filename, 'a', newline='') as f:
                     writer = csv.writer(f)
